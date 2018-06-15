33fec528abf610077c321a9ffacf1883282eb32a
Mikhail Efremov <mikhail.a.efremov@gmail.com>
2018-06-15 23:11:30 +0300

    Upload hello world project

---
+++ jam-unity-project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+  [SerializeField] private GameObject _stab;
+  [SerializeField] private GameObject _instance;
+
+  private void Awake()
+  {
+    _stab.SetActive(false);
+    
+    if(_instance == null)
+      throw new NullReferenceException("[Spawner] You need to set instance to spawn");
+
+    Instantiate(_instance, transform);
+  }
+}
