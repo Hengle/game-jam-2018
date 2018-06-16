94ca07c3dec804917d335d5c7bd737b4a4cbe712
Oleksandr Rogovskyi <o.rogovskyi@plarium.com>
2018-06-16 11:34:31 +0300

    Added movement script.

--- jam-unity-project/Assets/Space Shooter Project/_Complete-Game/Scripts/Done_PlayerController.cs
+++ jam-unity-project/Assets/Space Shooter Project/_Complete-Game/Scripts/Done_PlayerController.cs
@@ -9,6 +9,14 @@ public class Done_Boundary
 
 public class Done_PlayerController : MonoBehaviour
 {
+	private static Done_PlayerController _instance;
+	public static Done_PlayerController Instance
+	{
+		get
+		{
+			return _instance ?? (_instance = GameObject.Find("Done_Player").GetComponent<Done_PlayerController>());
+		}
+	}
 	public float speed;
 	public float tilt;
 	public Done_Boundary boundary;
@@ -21,29 +29,48 @@ public class Done_PlayerController : MonoBehaviour
 	
 	void Update ()
 	{
-		if (Input.GetButton("Fire1") && Time.time > nextFire) 
-		{
-			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			GetComponent<AudioSource>().Play ();
-		}
+//		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+//		{
+//			nextFire = Time.time + fireRate;
+//			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+//			GetComponent<AudioSource>().Play ();
+//		}
 	}
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical = Input.GetAxis ("Vertical");
+//
+//		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+//		GetComponent<Rigidbody>().velocity = movement * speed;
+//		
+//		GetComponent<Rigidbody>().position = new Vector3
+//		(
+//			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+//			0.0f, 
+//			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+//		);
+		
+		var rigidBody = GetComponent<Rigidbody>();
+		var rotation =  Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+		rotation.x = rigidBody.rotation.x;
+		rotation.y = rigidBody.rotation.y;
+		rigidBody.rotation = rotation;
+	}
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		GetComponent<Rigidbody>().velocity = movement * speed;
+	public void MoveHorizontal(float directionAndPower)
+	{
+		var rigid = GetComponent<Rigidbody>();
+		Vector3 movement = new Vector3 (directionAndPower, 0.0f, rigid.velocity.y);
+		rigid.velocity = movement * speed;
+	}
+
+	public void MoveVertical(float directionAndPower)
+	{
+		var rigid = GetComponent<Rigidbody>();
+		Vector3 movement = new Vector3 (rigid.velocity.x, 0.0f, directionAndPower);
+		rigid.velocity = movement * speed;
 		
-		GetComponent<Rigidbody>().position = new Vector3
-		(
-			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
-			0.0f, 
-			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
-		);
-		
-		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
 }
