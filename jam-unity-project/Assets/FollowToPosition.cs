4e9b020d7ad19763714bef9cc48305462f63b4ac
Mikhail Efremov <mikhail.a.efremov@gmail.com>
2018-06-17 00:17:12 +0300

    Added sprites

---
+++ jam-unity-project/Assets/FollowToPosition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowToPosition : MonoBehaviour
+{
+	[SerializeField] private GameObject _target;
+	
+	void LateUpdate ()
+	{
+		transform.position = new Vector3(_target.transform.position.x,
+			_target.transform.position.y + 2.5f,
+			_target.transform.position.z - 1.5f);
+	}
+}
