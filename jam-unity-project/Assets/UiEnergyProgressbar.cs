6628599d43b0052f432ceba707d923d9af9e9e95
Mikhail Efremov <mikhail.a.efremov@gmail.com>
2018-06-17 14:20:53 +0300

    Added miss

---
+++ jam-unity-project/Assets/UiEnergyProgressbar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UiEnergyProgressbar : MonoBehaviour
+{
+  [SerializeField] private Image _slider;
+  
+  private void Update()
+  {
+    var max = GameGod.Instance.MaximumEnergy;
+    var cur = GameGod.Instance.Energy;
+    
+    var value = ((float)cur / max);
+    
+    _slider.fillAmount = value;
+  }
+}
