1e1a942f57e9d842326e9c19b8f06e575c9ff478
Mikhail Efremov <mikhail.a.efremov@gmail.com>
2018-06-17 13:54:39 +0300

    merge

---
+++ jam-unity-project/Assets/BulletsUiProgressBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletsUiProgressBar : MonoBehaviour
+{
+  [SerializeField] private Image _slider;
+
+  private void Update()
+  {
+    var max = GameGod.Instance.MaximumBullets;
+    var cur = GameGod.Instance.CurrentBullets;
+    
+    var value = ((float)cur / max);
+    
+    _slider.fillAmount = value;
+  }
+}
