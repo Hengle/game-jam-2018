269dad0b69ce5edb46b2e9232ffa9817215b1989
Mikhail Efremov <mikhail.a.efremov@gmail.com>
2018-06-17 10:33:29 +0300

    Fix shield enable in ship

--- jam-unity-project/Assets/Scripts/Ship/ShieldController.cs
+++ jam-unity-project/Assets/Scripts/Ship/ShieldController.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class ShieldController : MonoBehaviour
 {
   private GameObject Shield;
+  private CameraFilterPack_AAA_SuperHexagon _shipPostEffect;
 
   public void Start()
   {
     Shield = transform.GetChild(0).gameObject;
+    _shipPostEffect = FindObjectOfType<CameraFilterPack_AAA_SuperHexagon>();
   }
   
   // Update is called once per frame
   void Update()
   {
-    if (GameGod.Instance.ShieldIsActivated && !Shield.activeInHierarchy)
+    if (GameGod.Instance.ShieldIsActivated)
     {
       Shield.SetActive(true);
+      _shipPostEffect.enabled = true;
     }
 
-    if (!GameGod.Instance.ShieldIsActivated && Shield.activeInHierarchy)
+    if (!GameGod.Instance.ShieldIsActivated)
     {
       Shield.SetActive(false);
+      _shipPostEffect.enabled = false;
     }
   }
 }
